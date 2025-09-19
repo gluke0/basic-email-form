e92ea77bbf4beef32b7120e6a4f6c29537774da7
gluke0 <milanesigianluca@gmail.com>
2025-09-19 16:56:17 +0200

    getting dom elements in js

--- main.js
+++ main.js
@@ -3,3 +3,7 @@
     // need to put my public key
     emailjs.init("EMAILJS_PUBLIC_KEY");
 })();
+
+// getting the DOM elements
+let contactForm = document.getElementById('contactForm');
+let successMessage = document.getElementById('success-message');
