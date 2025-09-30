ca36891d81b297c06cda24928a80f61fc545d69d
gluke0 <milanesigianluca@gmail.com>
2025-09-30 15:23:35 +0200

    getting infos from the form

--- main.js
+++ main.js
@@ -9,11 +9,15 @@ let contactForm = document.getElementById('contactForm');
 let successMessage = document.getElementById('success-message');
 
 contactForm.addEventListener('submit', function (event){
+    event.preventDefault();
 
+let formContent = {
+    to_name: "Admin",
+    sender_name: document.getElementById('name').value,
+    sender_email: document.getElementById('email').value,
+    subject: document.getElementById('subject').value,
+    message: document.getElementById('message').value,
+    reply_to: document.getElementById('email').value,
+};
 
-
-
-
-
-    
 });
